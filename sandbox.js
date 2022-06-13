ff00bd2eb5fdfab026d72402d8f3f3d319102663
Chris Mayes <cpmayes@gmail.com>
2022-06-13 16:11:23 -0400

    Part of the initial design doc drafted

---
+++ sandbox.js
@@ -0,0 +1,22 @@
+`Wordiness/Conciseness		
+Modifying Clauses		
+Inference		 	        
+Subject/Verb Agreement	
+Punctuation			        
+Verb Tense			
+Placement			        
+Omit/Delete			
+Yes/No or Keep/Delete		
+Possessives/Contractions             			
+Parallelism/Organization		
+Passive Voice
+Transitions			
+Ambiguity/Vagueness
+Pronouns			
+Comparisons
+Vocab in Context			
+Fixed Expressions
+Adjectives/Adverbs		
+Prepositions`.split('\n').forEach((line, i) => {
+    console.log(`${i+1}. ${line}`);
+})
